a80093cede25982f1b739c20f834357e4ba0b63d
Nikola Ivanov <ivanov.nikola826@gmail.com>
2026-06-18 20:13:59 +0200

    HÜ

--- 2026-04-09/script.js
+++ 2026-04-09/script.js
@@ -25,45 +25,60 @@ function renderPersons() {
     }
 }
 
+const sortState = {
+    id: 1,
+    name: 1,
+    groesse: 1,
+    geburtsdatum: 1,
+    herkunft: 1,
+    gewicht: 1
+};
+
 const thId = document.querySelector("#id");
 thId.addEventListener("click", () => {
     console.log("id clicked!!");
-    persons.sort((a, b) => a.id - b.id);
+    sortState.id *= -1;
+    persons.sort((a, b) => (a.id - b.id) * sortState.id);
     renderPersons();
 });
 
 const thName = document.querySelector("#name");
 thName.addEventListener("click", () => {
     console.log("name clicked!!");
-    persons.sort((a, b) => a.name.localeCompare(b.name));
+    sortState.name *= -1;
+    persons.sort((a, b) => a.name.localeCompare(b.name) * sortState.name);
     renderPersons();
 });
 
 const thHeight = document.querySelector("#groesse");
 thHeight.addEventListener("click", () => {
     console.log("height clicked!!");
-    persons.sort((a, b) => a.groesse - b.groesse);
+    sortState.groesse *= -1;
+    persons.sort((a, b) => (a.groesse - b.groesse) * sortState.groesse);
     renderPersons();
 });
 
 const thBirthday = document.querySelector("#geburtsdatum");
 thBirthday.addEventListener("click", () => {
     console.log("birthday clicked!!");
-    persons.sort((a, b) => a.geburtsdatum.localeCompare(b.geburtsdatum));
+    sortState.geburtsdatum *= -1;
+    persons.sort((a, b) => a.geburtsdatum.localeCompare(b.geburtsdatum) * sortState.geburtsdatum);
     renderPersons();
 });
 
 const thOrigin = document.querySelector("#herkunft");
 thOrigin.addEventListener("click", () => {
     console.log("origin clicked!!");
-    persons.sort((a, b) => a.herkunft.localeCompare(b.herkunft));
+    sortState.herkunft *= -1;
+    persons.sort((a, b) => a.herkunft.localeCompare(b.herkunft) * sortState.herkunft);
     renderPersons();
 });
 
 const thWeight = document.querySelector("#gewicht");
 thWeight.addEventListener("click", () => {
     console.log("weight clicked!!");
-    persons.sort((a, b) => a.gewicht - b.gewicht);
+    sortState.gewicht *= -1;
+    persons.sort((a, b) => (a.gewicht - b.gewicht) * sortState.gewicht);
     renderPersons();
 });
 
